01bbc47162ccb0e0ee09f455973ea0e381ec041d
Nastya Hlopovskaya <xlnastya@gmail.com>
2020-05-16 22:08:00 +0300

    login fixed

---
+++ app/src/main/java/test/itschool/samsung/ru/eco_lavka/Auth.java
@@ -0,0 +1,68 @@
+package test.itschool.samsung.ru.eco_lavka;
+
+import android.accounts.Account;
+import android.content.Intent;
+import android.util.Log;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Auth extends Thread {
+    private static String LOG_TAG = "MainActivity";
+    private final String baseUrl = "http://a274.sytes.net/php_server/";
+    public static int resp = -2;
+    Account account;
+
+    class Account {
+        private String login;
+        private String password;
+
+        public Account (String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
+    }
+
+    Auth(String login, String password) {
+        account = new Account(login, password);
+    }
+
+    @Override
+    public void run() {
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+
+            UserService userService = retrofit.create(UserService.class);
+            Call<Integer> userCall = userService.login(account.login, account.password);
+
+            userCall.enqueue(new Callback<Integer>() {
+                @Override
+                public void onResponse(Call<Integer> call, Response<Integer> response) {
+                    if (response.isSuccessful()) {
+                        resp = response.body();
+                        Log.v(LOG_TAG, "response " + resp);
+                    } else {
+                        Log.e(LOG_TAG, "response code " + response.code());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Integer> call, Throwable t) {
+                    Log.e(LOG_TAG, "failure " + t);
+                }
+            });
+    }
+}
--- app/src/main/java/test/itschool/samsung/ru/eco_lavka/LoginActivity.java
+++ app/src/main/java/test/itschool/samsung/ru/eco_lavka/LoginActivity.java
@@ -2,14 +2,20 @@ package test.itschool.samsung.ru.eco_lavka;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -26,27 +32,26 @@ import static java.lang.Thread.sleep;
 public class LoginActivity extends Activity {
 
     private static String LOG_TAG = "MainActivity";
-    private final String baseUrl = "http://a274.sytes.net/php_server/";
     private TextView answer;
     private String login, password;
     private EditText log, pass;
     int id = 0;
+    // -2 -> response hasn't come yet
+    // -1 -> connection problems, wrong data
+    // 0 -> wrong data
+    // any other number -> user id
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.login);
 
         //REGISTER
         TextView registerScreen = findViewById(R.id.link_to_register);
-        registerScreen.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-               //Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(i);
-            }
+        registerScreen.setOnClickListener(v -> {
+           //Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(i);
         });
 
         answer = findViewById(R.id.answer);
@@ -55,64 +60,59 @@ public class LoginActivity extends Activity {
 
         //LOGIN
         Button enterLogin = findViewById(R.id.btnLogin);
-        enterLogin.setOnClickListener(v -> {
-            sendPOST(v);
-
-
-
-            Log.v(LOG_TAG, "response main " + id);
-            if (id != -1 && id != 0) {
-                Intent intent=new Intent(LoginActivity.this, MainWidgets.class);
-                startActivity(intent);
-            }
-            else {
-                answer.setText("Логин или пароль введен неверно.");
-            }
-        });
+        enterLogin.setOnClickListener(this::sendPOST);
     }
 
     public void sendPOST(View view) {
-        login = log.getText().toString();
-        password = pass.getText().toString();
-        new MyAsyncTask().execute("");
+        login = log.getText().toString().trim();
+        password = pass.getText().toString().trim();
+
+        if (isFieldEmpty()) return;
+
+        answer.setText(null);
+        new SetAnswer();
+        answer.setHint("waiting..");
+        Thread thread = new Thread(new SetAnswer());
+        thread.start();
     }
 
-    @SuppressLint("StaticFieldLeak")
-    class MyAsyncTask extends AsyncTask<String, String, String> {
-
+    public class SetAnswer implements Runnable {
         @Override
-        protected String doInBackground(String... params) {
-
-            Gson gson = new GsonBuilder()
-                    .setLenient()
-                    .create();
-
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-
-            UserService userService = retrofit.create(UserService.class);
-            Call<Integer> userCall = userService.login(login, password);
-            userCall.enqueue(new Callback<Integer>() {
-                @Override
-                public void onResponse(Call<Integer> call, Response<Integer> response) {
-                    if (response.isSuccessful()) {
-                        id = response.body();
-                        Log.v(LOG_TAG, "response " + id);
-                    } else {
-                        Log.e(LOG_TAG,"response code " + response.code());
-                    }
+        public void run() {
+            synchronized (this) {
+                Auth auth = new Auth(login, password);
+                auth.start();
+                while (auth.resp == -2) {
+                    try {
+                        sleep(3000);
+                    } catch (InterruptedException e) {}
                 }
-
-                @Override
-                public void onFailure(Call<Integer> call, Throwable t) {
-                    Log.e(LOG_TAG,"failure " + t);
+                if (auth.resp == -1 || auth.resp == 0) {
+                    answer.setText(R.string.login_error);
+                    auth.resp = -2;
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainWidgets.class);
+                    startActivity(intent);
                 }
-            });
-            return null;
+            }
         }
-
     }
 
+    public void printError(EditText editText) {
+        editText.setHintTextColor(getResources().getColor(R.color.error));
+        editText.setHint(R.string.fill_the_field);
+    }
+
+    public boolean isFieldEmpty () {
+        boolean p = false;
+        if (login.equals("")) {
+            printError(log);
+            p = true;
+        }
+        if (password.equals("")) {
+            printError(pass);
+            p = true;
+        }
+        return p;
+    }
 }
