1c97c5d6ec7c09bbe29a01b3fe900a903fb268ea
Nastya Hlopovskaya <xlnastya@gmail.com>
2020-05-24 00:53:47 +0300

    chat updated

--- app/src/main/java/test/itschool/samsung/ru/eco_lavka/activities/Chat.java
+++ app/src/main/java/test/itschool/samsung/ru/eco_lavka/activities/Chat.java
@@ -2,8 +2,12 @@ package test.itschool.samsung.ru.eco_lavka.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,19 +18,34 @@ import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
+import android.text.format.DateFormat;
+import android.widget.Toast;
+
 import test.itschool.samsung.ru.eco_lavka.Message1;
 import test.itschool.samsung.ru.eco_lavka.R;
 
 public class Chat extends AppCompatActivity {
     private FirebaseListAdapter<Message1> adapter;
-    private LinearLayout chat;
+    private RelativeLayout chat;
+    private ImageButton send;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_widget);
-        chat = findViewById(R.id.chat);
-
+        chat = findViewById(R.id.chat_their_message);
+        send = findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textField = findViewById(R.id.editTextChat);
+                if (textField.getText().toString() == "") return;
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message1("Rose", textField.getText().toString()));
+                textField.setText("");
+                Toast.makeText(Chat.this, "send", Toast.LENGTH_SHORT).show();
+            }
+        });
         displayAllMessages();
     }
 
@@ -34,15 +53,23 @@ public class Chat extends AppCompatActivity {
         Query query = FirebaseDatabase.getInstance().getReference().child("chats");
         FirebaseListOptions<Message1> options = new FirebaseListOptions.Builder<Message1>()
                 .setQuery(query, Message1.class)
-                .setLayout(R.layout.my_message)
+                .setLayout(R.layout.their_message)
                 .build();
 
         ListView listMessages = findViewById(R.id.messages_view);
         adapter = new FirebaseListAdapter<Message1>(options) {
             @Override
             protected void populateView(View v, Message1 model, int position) {
-                //TextView message = chat.findViewById();
+                TextView user, text_message, message_time;
+                user = v.findViewById(R.id.name);
+                text_message = v.findViewById(R.id.message_body);
+                message_time = v.findViewById(R.id.message_time);
+
+                user.setText(model.getUserName());
+                text_message.setText(model.getTextMessage());
+                message_time.setText(DateFormat.format("HH:mm", model.getMessageTime()));
             }
         };
+        listMessages.setAdapter(adapter);
     }
 }
--- app/src/main/java/test/itschool/samsung/ru/eco_lavka/activities/MainWidgets.java
+++ app/src/main/java/test/itschool/samsung/ru/eco_lavka/activities/MainWidgets.java
@@ -4,42 +4,43 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import test.itschool.samsung.ru.eco_lavka.R;
-import test.itschool.samsung.ru.eco_lavka.cart.App;
-import test.itschool.samsung.ru.eco_lavka.cart.AppDatabase;
 import test.itschool.samsung.ru.eco_lavka.cart.CartActivity;
-import test.itschool.samsung.ru.eco_lavka.cart.AddProduct;
-import test.itschool.samsung.ru.eco_lavka.cart.GetCart;
 
 import static test.itschool.samsung.ru.eco_lavka.R.id.imageView;
 import static test.itschool.samsung.ru.eco_lavka.R.layout.main_widgets;
 
 public class MainWidgets extends AppCompatActivity {
-    private ImageView buttton;
+    private ImageView button;
     SharedPreferences sharedPreferences;
     final String SAVED_ID = "ID";
+    private ImageButton chat;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(main_widgets);
 
-        buttton = findViewById(imageView);
-        buttton.setOnClickListener(v -> {
+        button = findViewById(imageView);
+        button.setOnClickListener(v -> {
             Intent intent=new Intent(MainWidgets.this, Temp.class);
             startActivity(intent);
         });
 
-
+        chat = findViewById(R.id.messageButton);
+        chat.setOnClickListener(v -> {
+            Intent intent=new Intent(MainWidgets.this, Chat.class);
+            startActivity(intent);
+        });
 
         sharedPreferences = getSharedPreferences("user_setting", MODE_PRIVATE);
         Integer id = sharedPreferences.getInt(SAVED_ID, 0);
-
 
         ImageButton Catalog = findViewById(R.id.menuButton);
 
